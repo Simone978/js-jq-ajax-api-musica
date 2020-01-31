0a857c9e65869fc3db982482c89d55310d21236a
Simone <simonelega.sl@gmail.com>
2020-01-31 15:19:06 +0100

    dati album estrapolati e iniettati sulla pagina

--- main.js
+++ main.js
@@ -1,3 +1,26 @@
 $(document).ready(function() {
-	//Code
+  $.ajax(
+   {
+   url: "https://flynn.boolean.careers/exercises/api/array/music",
+   method: "GET",
+   success: function (data) {
+     albumWork(data.response)
+     },
+   error: function (richiesta, stato, errori) {
+     alert("E' avvenuto un errore. " + errori);
+     }
+   }
+  );
 });
+
+function albumWork(dati){
+  for (var i = 0; i < dati.length; i++) {
+    var cd= dati[i];
+    var source = document.getElementById("entry-template").innerHTML;
+    var template = Handlebars.compile(source);
+    var context = cd;
+    var html = template(context);
+    console.log(context);
+    $('.cds-container').append(html);
+    }
+}
